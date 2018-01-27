cc68d8f6afa6573d4dff8078bb4d36b97695f845
Eric Tran <me@erictran.ca>
2018-01-26 21:54:02 -0500

    Use CSS grid layout for the playing field

--- src/containers/PlayingField.tsx
+++ src/containers/PlayingField.tsx
@@ -5,6 +5,7 @@ import CardView from '../components/CardView';
 import ShopArea from './ShopArea';
 import CardGrid from '../components/CardGrid';
 import { StoreType } from '../models/Store';
+import { CardStackModelType } from '../models/Card';
 
 interface Props {
   name: string;
@@ -12,9 +13,12 @@ interface Props {
 }
 
 const StyledPlayingField = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 32px;
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-areas:
+    '. shop end-turn'
+    'discard-pile hand deck';
 `;
 
 const AreaTitle = styled.div`
@@ -24,28 +28,82 @@ const AreaTitle = styled.div`
   margin-bottom: 16px;
 `;
 
-const DeckArea = styled.div`
+const GridArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ShopGridArea = GridArea.extend`
+  grid-area: shop;
+`;
+
+const EndTurnGridArea = GridArea.extend`
+  grid-area: end-turn;
   display: flex;
   justify-content: center;
+  align-items: center;
+`;
+
+const HandGridArea = GridArea.extend`
+  grid-area: hand;
+`;
+
+const DiscardPileGridArea = GridArea.extend`
+  grid-area: discard-pile;
+`;
+
+const DeckGridArea = GridArea.extend`
+  grid-area: deck;
+`;
+
+const EndTurnButton = styled.button`
+  cursor: pointer;
+  width: 128px;
+  height: 64px;
+  font-size: 16px;
+  font-weight: bold;
 `;
 
 @inject('store')
 @observer
 class PlayingField extends React.Component<Props, object> {
-  displayDeck() {
-    return this.props.store!.deck.cards.map((card, i) => {
+  displayCards(cardStack: CardStackModelType) {
+    return cardStack.cards.map((card, i) => {
       return <CardView key={i} model={card} />;
     });
   }
   render() {
     return (
       <StyledPlayingField>
-        <AreaTitle>{this.props.name}</AreaTitle>
-        <ShopArea />
-        <AreaTitle>Deck</AreaTitle>
-        <DeckArea>
-          <CardGrid columns={5}>{this.displayDeck()}</CardGrid>
-        </DeckArea>
+        <ShopGridArea>
+          <ShopArea />
+        </ShopGridArea>
+
+        <EndTurnGridArea>
+          <EndTurnButton>End Turn</EndTurnButton>
+        </EndTurnGridArea>
+
+        <HandGridArea>
+          <AreaTitle>Hand</AreaTitle>
+          <CardGrid columns={5}>
+            {this.displayCards(this.props.store!.hand)}
+          </CardGrid>
+        </HandGridArea>
+
+        <DiscardPileGridArea>
+          <AreaTitle>Discard Pile</AreaTitle>
+          <CardGrid columns={3}>
+            {this.displayCards(this.props.store!.discardPile)}
+          </CardGrid>
+        </DiscardPileGridArea>
+
+        <DeckGridArea>
+          <AreaTitle>Deck</AreaTitle>
+          <CardGrid columns={3}>
+            {this.displayCards(this.props.store!.deck)}
+          </CardGrid>
+        </DeckGridArea>
       </StyledPlayingField>
     );
   }
