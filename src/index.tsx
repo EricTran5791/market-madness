f3281d741603b5b3b09b6e31913f81aa16578b54
Eric Tran <me@erictran.ca>
2018-02-03 14:04:59 -0500

    Add MobX React DevTools

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,14 @@ import './index.css';
 import { Provider } from 'mobx-react';
 import PlayingField from './containers/PlayingField';
 import { initializeStore } from './utils/gameStateManager';
+import DevTools from 'mobx-react-devtools';
 
 ReactDOM.render(
   <Provider store={initializeStore()}>
-    <PlayingField />
+    <>
+      <PlayingField />
+      {process.env.NODE_ENV === 'development' ? <DevTools /> : ''}
+    </>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
