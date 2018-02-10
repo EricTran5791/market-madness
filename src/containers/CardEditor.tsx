0cc9fc3df7084958b5f9927a0869da6f5a9afa37
Eric Tran <me@erictran.ca>
2018-02-10 15:45:18 -0500

    Added Office UI Fabric, initial implementation for the card editor

--- src/containers/CardEditor.tsx
+++ src/containers/CardEditor.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { CardModelSnapshotType, CardCategory } from '../models/Card';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import { ISelectableOption } from 'office-ui-fabric-react/lib/utilities/selectableOption/SelectableOption.types';
+
+interface State {
+  currentCard: CardModelSnapshotType;
+}
 
 const StyledCardEditor = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 3fr 2fr;
+  grid-gap: 16px;
   padding: 16px;
+`;
+
+const CardEditorSection = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.div`
@@ -13,11 +27,68 @@ const Title = styled.div`
   margin-bottom: 16px;
 `;
 
-class CardEditor extends React.Component<object, object> {
+const TextFieldContainer = styled.div`
+  textarea {
+    height: 60vh;
+    max-height: 60vh;
+  }
+`;
+
+class CardEditor extends React.Component<object, State> {
+  componentWillMount() {
+    this.setState({
+      currentCard: {},
+    });
+  }
+  updateCurrentCard(card: CardModelSnapshotType) {
+    this.setState({
+      currentCard: {
+        ...this.state.currentCard,
+        ...card,
+      },
+    });
+  }
   render() {
     return (
       <StyledCardEditor>
-        <Title>Card Editor</Title>
+        <CardEditorSection>
+          <Title>Card Editor</Title>
+          <TextField
+            label="Name"
+            onChanged={(value: string) => {
+              this.updateCurrentCard({ name: value });
+            }}
+          />
+
+          <Dropdown
+            label="Category"
+            options={Object.keys(CardCategory).map((key, i) => {
+              const category = CardCategory[key];
+              return { key: i, text: category };
+            })}
+            onChanged={({ text }: ISelectableOption) => {
+              this.updateCurrentCard({ category: text });
+            }}
+          />
+
+          <TextField
+            label="Description"
+            onChanged={(value: string) => {
+              this.updateCurrentCard({ description: value });
+            }}
+          />
+        </CardEditorSection>
+
+        <CardEditorSection>
+          <Title>Card JSON</Title>
+          <TextFieldContainer>
+            <TextField
+              multiline
+              autoAdjustHeight
+              value={JSON.stringify(this.state.currentCard, undefined, 2)}
+            />
+          </TextFieldContainer>
+        </CardEditorSection>
       </StyledCardEditor>
     );
   }
