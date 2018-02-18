42bfc5dc3353a5836f0e76a777643a756bfb58ef
Eric Tran <me@erictran.ca>
2018-02-18 15:30:48 -0500

    Add validation for card library operations, added a message bar to show the success and error messages

--- src/models/CardLibrary.ts
+++ src/models/CardLibrary.ts
@@ -4,6 +4,31 @@ import cardLibrary from '../utils/cardLibrary.json';
 import { CardLibrary as CardLibraryRecordType } from '../types/cardTypes';
 import { printCardByIdNew } from '../utils/cardGenerator';
 
+export enum CardLibraryOperationKind {
+  Success = 'Success',
+  Error = 'Error',
+}
+
+export type CardLibraryOperation = {
+  kind: CardLibraryOperationKind;
+  text: string;
+};
+
+enum CardValidationStatusKind {
+  Valid = 'Valid',
+  Invalid = 'Invalid',
+}
+
+type CardValidationStatus = {
+  kind: CardValidationStatusKind;
+  text: string;
+};
+
+type ValidateCardParams = {
+  card: CardModelType;
+  targetId: string;
+};
+
 export const CardLibrary = types
   .model('CardLibrary', {
     cards: types.optional(types.array(Card), []),
@@ -12,12 +37,12 @@ export const CardLibrary = types
     get cardLibraryJson() {
       return self.cards.map(_ => _.cardJson).join(',\n');
     },
-    getCardById(id: string) {
+    getCardById(id: string): CardModelType {
       return self.cards.find(_ => _.id === id);
     },
   }))
-  .actions(self => ({
-    afterCreate() {
+  .actions(self => {
+    function afterCreate() {
       const library = cardLibrary as CardLibraryRecordType;
       applySnapshot(
         self.cards,
@@ -25,33 +50,90 @@ export const CardLibrary = types
           return printCardByIdNew(id);
         })
       );
-    },
-    updateCard(id: string, snapshot: CardModelSnapshotType): boolean {
-      // If the id was changed in the snapshot, check that there isn't a card with the same id as the snapshot
-      const duplicateId = id !== snapshot.id && self.getCardById(snapshot.id);
-      const updatedCard = self.cards.find(_ => _.id === id);
-      if (updatedCard && !duplicateId) {
-        applySnapshot(updatedCard, snapshot);
-        return true;
+    }
+
+    function updateCard(
+      id: string,
+      snapshot: CardModelSnapshotType
+    ): CardLibraryOperation {
+      const cardToUpdate = self.getCardById(id);
+      const cardValidationStatus = validateCard({
+        card: cardToUpdate,
+        // If the id was changed in the snapshot, check that there isn't a card with the same id as the snapshot
+        targetId: id !== snapshot.id ? snapshot.id : undefined,
+      });
+
+      if (cardValidationStatus.kind === CardValidationStatusKind.Valid) {
+        applySnapshot(cardToUpdate, snapshot);
+        return {
+          kind: CardLibraryOperationKind.Success,
+          text: `Success! ${
+            snapshot.name
+          } has been updated in the card library.`,
+        };
       } else {
-        return false;
+        return {
+          kind: CardLibraryOperationKind.Error,
+          text: cardValidationStatus.text,
+        };
       }
-    },
-    addCard(card: CardModelType): boolean {
-      const duplicateId = self.getCardById(card.id);
-      if (!duplicateId) {
+    }
+
+    function addCard(card: CardModelType): CardLibraryOperation {
+      const cardValidationStatus = validateCard({ card, targetId: card.id });
+      if (cardValidationStatus.kind === CardValidationStatusKind.Valid) {
         self.cards.push(card);
-        return true;
+        return {
+          kind: CardLibraryOperationKind.Success,
+          text: `Success! ${card.name} has been added to the card library.`,
+        };
       } else {
-        return false;
+        return {
+          kind: CardLibraryOperationKind.Error,
+          text: cardValidationStatus.text,
+        };
       }
-    },
-    deleteCard(id: string) {
+    }
+
+    function deleteCard(id: string) {
       const deletedCard = self.cards.find(_ => _.id === id);
       if (deletedCard) {
         self.cards.remove(deletedCard);
       }
-    },
-  }));
+    }
+
+    function validateCard({
+      card,
+      targetId,
+    }: ValidateCardParams): CardValidationStatus {
+      // Check to see if there is another card in the library with the same id
+      if (targetId && self.getCardById(targetId)) {
+        return {
+          kind: CardValidationStatusKind.Invalid,
+          text:
+            'There is another card in the library with the same id. Please use another name.',
+        };
+      }
+
+      // Check for an empty id
+      if (card.id === '') {
+        return {
+          kind: CardValidationStatusKind.Invalid,
+          text:
+            'Error! The card id must not be empty. Please change the card name.',
+        };
+      }
+
+      return { kind: CardValidationStatusKind.Valid, text: '' };
+    }
+
+    return {
+      afterCreate,
+      updateCard,
+      addCard,
+      deleteCard,
+      validateCard,
+    };
+  });
 
 export type CardLibraryModelType = typeof CardLibrary.Type;
